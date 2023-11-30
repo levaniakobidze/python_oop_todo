9fde995ac769a5f17e20d86e8cfa6e7325e9b7aa
levaniakobidze <levaniakobidze25@gmail.com>
2023-11-30 20:44:09 +0400

    feature:(TODO):add remove and view_all_tasks functions

--- todo.py
+++ todo.py
@@ -8,6 +8,23 @@ class Todo():
         self.tasks.append(task)
         print(f"Task '{task} added'")
         
+    def remove_task(self,task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+            print(f"Task '{task} deleted'")
+        else:
+            print("Task not found.")   
+            
+    def view_all_tasks():
+        if self.tasks:
+            print("Your tasks:")
+            for index,task in enumerate(self.tasks,start=1):
+                print(f"{index}, {task}")
+            else:
+                print("No tasks yet.")
+        
+        
      
 if __name__ == "__main__":
     todo_app = Todo()   
+  
